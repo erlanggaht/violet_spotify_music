1244cbccdcebcbf0663e4432bf53fa84c19c39f4
Erlangga Hidayatullah <erlanggahidayat.md@gmail.com>
2024-11-22 00:16:30 +0700

    shadcn configuration and adding color variable darklategrey in global.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,32 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      width: {
-        header_left_right: '334px'
-      }
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+			darkslategrey: 'var(--darklategrey)'
+  		},
+  		width: {
+  			header_left_right: '334px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)',
+			search: '36px'
+  		},
+		fontSize: {
+			sm: '16px'
+		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
