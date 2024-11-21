5ed717d6a97cf5e8cb6d10260d6437334a398b1a
Erlangga Hidayatullah <erlanggahidayat.md@gmail.com>
2024-11-22 00:14:36 +0700

    feat: import layout of the git submodule component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Layout from '@/app/components/layout'
+import Layout from '@/app/violet_spotify_music_components_submodule/layout'
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
