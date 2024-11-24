6f4c8cd8cb63fa14fde71c715887d5e0c255e175
Erlangga Hidayatullah <erlanggahidayat.md@gmail.com>
2024-11-24 15:36:43 +0700

    feat: add session profile chore: decalare module default session in nextauth
    (cherry picked from commit 838065fb602e58d89f97034bc7046ae5a52bc38b)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth, { AuthOptions, DefaultSession, ISODateString } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
 export const authOptions: AuthOptions = {
@@ -9,24 +9,37 @@ export const authOptions: AuthOptions = {
       authorization: {
         url: "https://accounts.spotify.com/authorize",
         params: {
-          scope: "user-read-private user-read-email user-read-recently-played user-top-read",
+          scope:
+            "user-read-private user-read-email user-read-recently-played user-top-read",
         },
-      }
+      },
     }),
   ],
   callbacks: {
-    async session({ session, token, user } : {
-        session: any,
-        token: any,
-        user: any
+    async session({
+      session,
+      token,
+      user,
+    }: {
+      session: any;
+      token: any;
+      user: any;
     }) {
-      session.token = token.access_token
+      if (token?.profile) {
+        session.user = token.profile;
+        session.user.name = token.profile.display_name;
+      } else {
+        session.user = user; 
+      }
+
+      session.token = token.access_token;
       return session;
     },
     async jwt({ token, user, account, profile }) {
-        if(account) {
-            token.access_token = account.access_token
-        }
+      if (account || user) {
+        token.profile = profile;
+        token.access_token = account?.access_token;
+      }
       return token;
     },
   },
@@ -35,3 +48,22 @@ export const authOptions: AuthOptions = {
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
+// Addition of the DefaultSession custom type
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user?: {
+      name?: string | null
+      email?: string | null
+      image?: string | null
+      images?: {
+        url: string,
+        height: number,
+        width: number
+      }[],
+      product: string
+
+    }
+    expires: ISODateString,
+  }
+}
