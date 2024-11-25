dfa569c2dd51d5233768aafeaecad2f7f17c73a1
Erlangga Hidayatullah <erlanggahidayat.md@gmail.com>
2024-11-25 23:34:13 +0700

    chore: constant screen media query

---
+++ src/lib/contants/index.ts
@@ -0,0 +1,7 @@
+const Constant_ScreenMediaQuery = {
+  desktop: "(min-width: 1280px)", // >
+  tablet: "(min-width: 750px)", // >
+  mobile: "(max-width: 750px)", // <
+};
+
+export { Constant_ScreenMediaQuery };
