0b5789e32585f51c5f8c796010242ff593241fbb
Erlangga Hidayatullah <erlanggahidayat.md@gmail.com>
2024-11-27 21:56:42 +0700

    feat: constants

---
+++ src/lib/contants/index.ts
@@ -0,0 +1,13 @@
+const Constant_ScreenMediaQuery = {
+  desktop: "(min-width: 1280px)", // >
+  tablet: "(min-width: 750px)", // >
+  mobile: "(max-width: 750px)", // <
+};
+
+const Constant_Screen = {
+  desktop:  1280,
+  tablet: 750, // >
+  mobile: 750, // <
+};
+
+export { Constant_ScreenMediaQuery, Constant_Screen };
